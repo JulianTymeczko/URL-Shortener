dd5cba1533b4e3615fda89e4e18f8a356b1ae381
Julian Houle-T <jhouletymeczko@gmail.com>
2023-07-02 01:09:04 -0400

    finished dynamic javascript still need to add if URL is blank

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
+let shortenIt = document.getElementById("search-bar-button")
+let userInput = document.querySelector("input")
+let pastSearchesTop = document.getElementById("statistics-top")
 
 
-let shorteningURL = `https://api.shrtco.de/v2/shorten?url=${userInput.value}`
+shortenIt.addEventListener("click", function () {
+    let pastSearches = document.createElement("div")
+    pastSearches.setAttribute("class", "past-searches")
+    pastSearchesTop.insertAdjacentElement("afterend", pastSearches)
+    let fullURL = document.createElement("h1")
+    fullURL.setAttribute("id", "full-URL")
+    let shortURL = document.createElement("h2")
+    shortURL.setAttribute("id", "short-URL")
+    let copyButton = document.createElement("button")
+    copyButton.setAttribute("id", "copy-button")
+    copyButton.textContent = "Copy"
+
+
+    pastSearches.appendChild(fullURL)
+    pastSearches.appendChild(shortURL)
+    pastSearches.appendChild(copyButton)
+
+    let shorteningURL = `https://api.shrtco.de/v2/shorten?url=${userInput.value}`
     fetch(shorteningURL)
         .then(function (response) {
             return response.json()
         })
-        .then (function (data){
+        .then(function (data) {
+            fullURL.textContent = userInput.value
+            shortURL.textContent = data.result.short_link
             console.log(data)
-            console.log(data.result.short_link)  
+            console.log(data.result.short_link)
+            copyButton.addEventListener("click", function () {
+                copyButton.textContent = "Copied!"
+                copyButton.setAttribute("style", "background-color: hsl(257, 27%, 26%);")
+
+            })
         })
 
+})
 
-        
+
+
+
